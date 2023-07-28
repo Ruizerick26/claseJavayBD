2ca458bd05b8218d4dc64e16b3c6dc67bc337b3b
Ruizerick26 <ruizerick2525@gmail.com>
2023-07-28 11:24:01 -0500

    336

--- src/bdjava.java
+++ src/bdjava.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class bdjava {
+import java.sql.*;
+public class bdjava {
+    static final String db_URL="jdbc:mysql://localhost/POO1";
+    static final String usser="root";
+    static final String pass="root_bas3";
+    static final String query="SELECT * from estudiantes";
+    public static void main(String[] args) {
+    try(
+            Connection conn = DriverManager.getConnection(db_URL,usser,pass);
+            Statement stat = conn.createStatement();
+            ResultSet rs = stat.executeQuery(query);
+            ){
+        while (rs.next()) {
+
+            System.out.println("id:" + rs.getString("IDest"));
+            System.out.println("Nombre:" + rs.getString("Nombreest"));
+
+
+        }
+
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    }
+    }
 }
--- src/dbfront.java
+++ src/dbfront.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class dbfront {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class dbfront {
+    private JButton queryButton;
+    private JTextArea info;
+    private JPanel root;
+    static final String db_URL="jdbc:mysql://localhost/POO1";
+    static final String usser="root";
+    static final String pass="root_bas3";
+    static final String query="SELECT * from estudiantes";
+
+    public dbfront() {
+        queryButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try(
+                        Connection conn = DriverManager.getConnection(db_URL,usser,pass);
+                        Statement stat = conn.createStatement();
+                        ResultSet rs = stat.executeQuery(query);
+                ){
+                    while (rs.next()) {
+
+                        System.out.println("id:" + rs.getString("IDest"));
+                        System.out.println("Nombre:" + rs.getString("Nombreest"));
+                        info.append("id:" + rs.getString("IDest") + "-Nombre:" + rs.getString("Nombreest")+ "-edad" + rs.getString("EdadEst") + "\n");
+
+
+                    }
+
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("dbfront");
+        frame.setContentPane(new dbfront().root);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
+
